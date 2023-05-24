3e25e2718afda5177ce86ad184cb51204007c317
박주형 <rlfkelddk12@gmail.com>
2023-05-24 05:30:56 +0000

    확인

--- js/login.js
+++ js/login.js
@@ -16,16 +16,22 @@ function login(){
 		setCookie("id", id.value, 0);
 	}
 	
-	if(id.vlaue.length === 0 || password.value.length === 0){
+	if(id.value.length === 0 || password.value.length === 0){
 		alert("아이디와 비밀번호를 모두 입력해주세요.");
 	}else{
+		session_set();
 		form.submit();
 	}
 }
 function logout(){
+	session_del();
 	location.href='../index.html';
 }
 function get_id(){
+	if(true){
+		decrypt_text();
+	}
+	else{
 	var getParameters = function(paramName){
 	var returnValue;
 	var url = location.href;
@@ -40,11 +46,12 @@ function get_id(){
 		}
 	}
 	alert(getParameters('id') + '님 반갑습니다!');
+	}
 }
 function deleteCookie(cookieName){
 	var expireDate = new Date();
 	expireDate.setDate(expireDate.getDate() -1);
-	document.cookie = cookieName + "= " + "; expires=" +expireDate.toGMTStirng();
+	document.cookie = cookieName + "= " + ";expires=" + expireDate.toGMTStirng();
 }
 function init(){
 	let id = document.querySelector("#floatingInput");
@@ -55,4 +62,99 @@ function init(){
 		id.value = get_id;
 		check.checked = true;
 	}
+	session_check();
 }
+function session_set(){ //세션 저장(객체)    
+    let id = document.querySelector("#floatingInput");
+    let password = document.querySelector("#floatingPassword");
+    let random = new Date(); // 랜덤 타임스탬프
+    
+    const obj = { // 객체 선언
+    id : id.value,
+    otp : random
+    }
+    
+    if (sessionStorage) {
+        const objString = JSON.stringify(obj); // 객체 -> JSON 문자열 변환
+        let en_text = encrypt_text(objString); // 암호화
+        sessionStorage.setItem("Session_Storage_object", objString);
+        sessionStorage.setItem("Session_Storage_encrypted", en_text);
+    } else {
+        alert("세션 스토리지 지원 x");
+    }   
+}
+/*function session_set(){
+	let id = document.querySelector("#floatingInput");
+	let password = document.querySelector("#floatingPassword");
+	if (sessionStorage){
+		let en_text = encrypt_text(password.vlaue);
+		sessionStorage.setItem("Session_Storage_test",en_text);
+	} else {
+		alert("로컬 스토리지 지원 x");
+	}
+}*/
+function session_get(){
+	if (sessionStorage){
+	return sessionStorage.getItem("Session_Storage_encrypted");
+} else {
+	alert("세션 스토리지 지원 x");
+	}
+}
+function session_check(){
+	if (sessionStorage.getItem("Session_Storage_test")){
+		alert("이미 로그인 되었습니다.");
+	}
+}
+function session_del() {
+	// Check if the sessionStorage object exist
+	if (sessionStorage) {
+		//Retrieve data
+		sessionStorage.removeItem("Session_Storage_test");
+		alert("로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.");
+	} else {
+		alert("세션 스토리지 지원 x")
+	}
+}
+function encodeByAES256(key, data){
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString();
+}
+
+function decodeByAES256(key, data){
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+};
+function encrypt_text(password){
+    const k = "key"; // 클라이언트 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const b = password;
+    const eb = this.encodeByAES256(rk, b);
+    return eb;
+    console.log(eb);
+}
+
+function decrypt_text(){
+    const k = "key"; // 서버의 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const eb = session_get();
+    const b = this.decodeByAES256(rk, eb);
+    console.log(b); 
+}
+function addjavascript(isname) {
+	var th = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.setAttribute('type','text/javascript');
+	s.setAttribute('src',jsname);
+	th.appendChild(s);
+}
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,7 +1,7 @@
 function pop_up(){
 	var cookieCheck = getCookie("popupYN");
 	if (cookieCheck != "N"){
-	window.open("../pop_up/pop_up.html", "팝업테스트","width=400, height=300,top=10,left=10");
+	window.open("pop_up.html", "팝업테스트","width=400, height=300,top=10,left=10");
 	}
 }
 function showclock(){
@@ -30,7 +30,6 @@ function setCookie(name, vlaue, expiredays){
 	var date = new Date();
 	date.setDate(date.getDate() + expiredays);
 	document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";
-		date.toUTCString();
 }
 function getCookie(name){
 	var cookie = document.cookie;
